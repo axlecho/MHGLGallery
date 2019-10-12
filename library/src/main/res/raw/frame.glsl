9b09f93f50f34e2440454e9731815eb71fb09541
axlecho <axlecho@gmail.com>
2019-10-13 04:11:40 +0800

    [feature] move to pcf

--- library/src/main/res/raw/frame.glsl
+++ library/src/main/res/raw/frame.glsl
@@ -1,41 +1,62 @@
+// Based on http://blog.shayanjaved.com/2011/03/13/shaders-android/
+// from Shayan Javed
+// And dEngine source from Fabien Sanglard
+
 precision mediump float;
 uniform sampler2D uTexture;
 varying vec2 vTextureCoordinate;
 
 
-// The position of the light in eye space.
-uniform vec3 uLightPos;
-
 // Texture variables: depth texture
 uniform sampler2D uShadowTexture;
+
+// This define the value to move one pixel left or right
+uniform float uxPixelOffset;
+// This define the value to move one pixel up or down
+uniform float uyPixelOffset;
 
 // shadow coordinates
 varying vec4 vShadowCoord;
 
-float shadowSimple()
+float lookup( vec2 offSet)
 {
     vec4 shadowMapPosition = vShadowCoord / vShadowCoord.w;
 
-    float distanceFromLight = texture2D(uShadowTexture, shadowMapPosition.st).z;
+    float distanceFromLight = texture2D(uShadowTexture, (shadowMapPosition +
+    vec4(offSet.x * uxPixelOffset, offSet.y * uyPixelOffset, 0.05, 0.0)).st ).z;
 
     //add bias to reduce shadow acne (error margin)
     float bias = 0.0005;
 
-    //1.0 = not in shadow (fragmant is closer to light than the value stored in shadow map)
-    //0.0 = in shadow
     return float(distanceFromLight > shadowMapPosition.z - bias);
 }
 
+float shadowPCF()
+{
+    float shadow = 1.0;
 
-void main(){
+    for (float y = -1.5; y <= 1.5; y = y + 1.0) {
+        for (float x = -1.5; x <= 1.5; x = x + 1.0) {
+            shadow += lookup(vec2(x,y));
+        }
+    }
+
+    shadow /= 16.0;
+    shadow += 0.2;
+
+    return shadow;
+}
+
+void main()
+{
 
     // Shadow
-      float shadow = 1.0;
+    float shadow = 1.0;
 
-    // if the fragment is not behind light view frustum
+    //if the fragment is not behind light view frustum
     if (vShadowCoord.w > 0.0) {
 
-        shadow = shadowSimple();
+        shadow = shadowPCF();
 
         //scale 0.0-1.0 to 0.2-1.0
         //otherways everything in shadow would be black
